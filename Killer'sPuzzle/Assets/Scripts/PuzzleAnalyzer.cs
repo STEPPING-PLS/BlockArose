feb7fcde5adda931a3f19d000c2949ac79e64e96
kz-rv04 <k.kobayashi0426@gmail.com>
2017-03-24 01:04:35 +0900

    03/24
    デバッグ用にお遊び程度のAI追加、バグを引き起こす可能性あり

--- Killer'sPuzzle/Assets/Scripts/PuzzleAnalyzer.cs
+++ Killer'sPuzzle/Assets/Scripts/PuzzleAnalyzer.cs
@@ -39,6 +39,7 @@ public class PuzzleAnalyzer : MonoBehaviour {
     {
         if (this.state == AIState.SLEEPING) {
             print("Activate");
+            this.state = AIState.ANALYZING;
             StartCoroutine(AnalyzePuzzle());
         }
     }
@@ -47,52 +48,59 @@ public class PuzzleAnalyzer : MonoBehaviour {
     {
         if (this.state == AIState.ANALYZING) {
             print("Sleep");
+            this.state = AIState.SLEEPING;
             StopCoroutine(AnalyzePuzzle());
         }
     }
 
     IEnumerator AnalyzePuzzle()
     {
-        while (true)
+        while (state == AIState.ANALYZING)
         {
             // 消すパネルが見つかるまで実行
-            while (!SolvePuzzle(Random.Range(0, sys.stage.GetLength(0)), Random.Range(0, sys.stage.GetLength(0)))) ;
+            for(;!SolvePuzzle(Random.Range(0, sys.stage.GetLength(0)), Random.Range(0, sys.stage.GetLength(0)));)
+                yield return new WaitForEndOfFrame();
             yield return new WaitForSeconds(ExecSpeed);
         }
+        yield return 0;
     }
 
     private bool SolvePuzzle(int i,int j)
     {
         if (sys.stage[i, j] == null) return false;
+        Block selected = null;
+        Block dest = null;
         // ステージのブロックの色を配列に格納
         BlockType[,] colors = new BlockType[sys.stage.GetLength(0), sys.stage.GetLength(1)];
         for (int x = 0; x < sys.stage.GetLength(0); x++)
         {
             for (int y = 0; y < sys.stage.GetLength(1); y++)
             {
-                if (sys.stage[x,y] != null && sys.stage[x,y].GetComponent<Block>().BlockStatus == BlockStatus.NORMAL)
+                if (sys.stage[x, y] != null)
                     colors[x, y] = sys.stage[x, y].GetComponent<Block>().BlockType;
             }
         }
+        if ((selected = sys.stage[i, j].GetComponent<Block>()).BlockStatus != BlockStatus.NORMAL)
+            return false;
         // 上2マスが同じ色の場合
         if (i - 1 > 0 && colors[i - 1, j] == colors[i - 2, j])
         {
             // 左のマスが上2マスと同じ色の場合
             if (j > 0 && colors[i - 1, j] == colors[i, j - 1]) {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i - 1, j].GetComponent<Block>(), sys.stage[i, j].GetComponent<Block>()));
-                return true;
+                if(sys.stage[i,j - 1] != null)
+                    dest = sys.stage[i, j - 1].GetComponent<Block>();
             }
             // 下のマスが上2マスと同じ色
             else if (i + 1 < sys.stage.GetLength(0) && colors[i - 1, j] == colors[i + 1, j])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i - 1, j].GetComponent<Block>(), sys.stage[i,j].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i + 1, j] != null)
+                    dest = sys.stage[i + 1, j].GetComponent<Block>();
             }
             // 右のマスが上2マスと同じ色
             else if (j + 1 < sys.stage.GetLength(1) && colors[i - 1, j] == colors[i, j + 1])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i, j + 1].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i, j + 1] != null)
+                    dest = sys.stage[i, j + 1].GetComponent<Block>();
             }
         }
         //以下同様に判定を行う
@@ -101,18 +109,18 @@ public class PuzzleAnalyzer : MonoBehaviour {
         {
             if (i > 0 && colors[i, j - 1] == colors[i - 1, j])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i - 1, j].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i - 1, j] != null)
+                    dest = sys.stage[i - 1, j].GetComponent<Block>();
             }
             else if (i + 1 < sys.stage.GetLength(0) && colors[i, j - 1] == colors[i + 1, j])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i + 1, j].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i + 1, j] != null)
+                    dest = sys.stage[i + 1, j].GetComponent<Block>();
             }
             else if (j + 1 < sys.stage.GetLength(1) && colors[i, j - 1] == colors[i, j + 1])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i, j + 1].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i, j + 1] != null)
+                    dest = sys.stage[i, j + 1].GetComponent<Block>();
             }
         }
         // 下2マスが同じ色の場合
@@ -120,18 +128,18 @@ public class PuzzleAnalyzer : MonoBehaviour {
         {
             if (i > 0 && colors[i + 1, j] == colors[i - 1, j])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i - 1, j].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i - 1, j] != null)
+                    dest = sys.stage[i - 1, j].GetComponent<Block>();
             }
             else if (j > 0 && colors[i + 1, j] == colors[i, j - 1])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i, j - 1].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i, j - 1] != null)
+                    dest = sys.stage[i, j - 1].GetComponent<Block>();
             }
             else if (j + 1 < sys.stage.GetLength(1) && colors[i + 1, j] == colors[i, j + 1])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i, j + 1].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i, j + 1] != null)
+                    dest = sys.stage[i, j + 1].GetComponent<Block>();
             }
         }
         // 右2マスが同じ色の場合
@@ -139,18 +147,18 @@ public class PuzzleAnalyzer : MonoBehaviour {
         {
             if (i > 0 && colors[i, j + 1] == colors[i - 1, j])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i - 1, j].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i - 1, j] != null)
+                    dest = sys.stage[i - 1, j].GetComponent<Block>();
             }
             else if (j > 0 && colors[i, j + 1] == colors[i, j - 1])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i, j - 1].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i, j - 1] != null)
+                    dest = sys.stage[i, j - 1].GetComponent<Block>();
             }
             else if (i + 1 < sys.stage.GetLength(1) && colors[i, j + 1] == colors[i + 1, j])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i + 1, j].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i + 1, j] != null)
+                    dest = sys.stage[i + 1, j].GetComponent<Block>();
             }
         }
         // 上下2マスが同じ色の場合
@@ -158,13 +166,13 @@ public class PuzzleAnalyzer : MonoBehaviour {
         {
             if (j > 0 && colors[i - 1, j] == colors[i, j - 1])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i, j - 1].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i, j - 1] != null)
+                    dest = sys.stage[i, j - 1].GetComponent<Block>();
             }
             else if (j + 1 < sys.stage.GetLength(1) && colors[i - 1, j] == colors[i, j + 1])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i, j + 1].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i, j + 1] != null)
+                    dest = sys.stage[i, j + 1].GetComponent<Block>();
             }
         }
         // 左右2マスが同じ色の場合
@@ -172,12 +180,21 @@ public class PuzzleAnalyzer : MonoBehaviour {
         {
             if (i > 0 && colors[i, j - 1] == colors[i - 1, j])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i - 1, j].GetComponent<Block>()));
-                return true;
+                if (sys.stage[i - 1, j] != null)
+                    dest = sys.stage[i - 1, j].GetComponent<Block>();
             }
             else if (i + 1 < sys.stage.GetLength(1) && colors[i, j - 1] == colors[i + 1, j])
             {
-                sys.StartCoroutine(sys.SwitchBlock(sys.stage[i, j].GetComponent<Block>(), sys.stage[i + 1, j].GetComponent<Block>()));
+                if (sys.stage[i + 1, j] != null)
+                    dest = sys.stage[i + 1, j].GetComponent<Block>();
+
+            }
+        }
+        if (dest != null)
+        {
+            if (dest.BlockStatus == BlockStatus.NORMAL)
+            {
+                sys.StartCoroutine(sys.SwitchBlock(selected, dest));
                 return true;
             }
         }
